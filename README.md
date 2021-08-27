b7bd64bd27ac6f8cf25155b51b794c6369ebc007
Chan-ZJU <1072122585@qq.com>
2021-08-27 23:14:36 +0800

    My first git commit of my dotfiles

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ## dotfiles
 
 The repository to store my dotfiles.
+
+### Installation
+
+```bash
+git clone git://github.com/Chan-ZJU/dotfiles ~/dotfiles
+cd ~/dotfiles
+./install.sh
+```
---
+++ install.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+# this script creates symlinks from ~ to any dotfiles in the dotfiles directory
+
+#### Variables
+dir=~/dotfiles
+olddir=~/dotfiles_old
+files="bashrc p10k.zsh vim vscode git profile gitconfig vimrc zshrc oh-my-zsh"
+
+# create dotfiles_old in homedir
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "...done"
+
+# change to the dotfiles directory
+echo "Changing to the $dir directory"
+cd $dir
+echo "...done"
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
+for file in $files; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file ~/dotfiles_old/
+    echo "Creating symlink to $file in home directory."
+    ln -sf $dir/.$file ~/.$file
+done
+
+install_zsh() {
+    # Test to see if zshell is installed.  If it is:
+    if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
+        # Clone my oh-my-zsh repository from GitHub only if it isn't already present
+        if [[ ! -d $dir/oh-my-zsh/ ]]; then
+            git clone http://github.com/robbyrussell/oh-my-zsh.git
+        fi
+        # Set the default shell to zsh if it isn't currently set to zsh
+        if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+            chsh -s $(which zsh)
+        fi
+    else
+        # If zsh isn't installed, get the platform of the current machine
+        platform=$(uname)
+        # If the platform is Linux, try an apt-get to install zsh and then recurse
+        if [[ $platform == 'Linux' ]]; then
+            if [[ -f /etc/redhat-release ]]; then
+                sudo yum install zsh
+                install_zsh
+            fi
+            if [[ -f /etc/debian_version ]]; then
+                sudo apt-get install zsh
+                install_zsh
+            fi
+        # If the platform is OS X, tell the user to install zsh :)
+        elif [[ $platform == 'Darwin' ]]; then
+            echo "Please install zsh, then re-run this script!"
+            exit
+        fi
+    fi
+}
+
+install_zsh
